e5a6a9a2b4a0ede15594a41a65342b87a1d66895
Darvesh Gorhe <darveshgorhe@gmail.com>
2024-07-22 13:58:22 -0400

    style: replaced commented mypy type hints with actual mypy type hints

--- data.py
+++ data.py
@@ -10,7 +10,7 @@ from colors import EdgeColors, LifecycleColorsDict, LifecycleColors
 import controls
 
 from itertools import permutations
-from typing import Dict, Hashable, List, Set
+from typing import Dict, Hashable, List, Set, Union
 from pdb import set_trace
 
 
@@ -95,23 +95,22 @@ def subset_by_node_type(df: DataFrame) -> DataFrame:
         "SEC": EdgeColors.SEC.value,
         "both": EdgeColors.Both.value
     })
-    datasets = dataset_checkbox_button.active  # List[int]
-    # List[int]
-    ip_and_sec_toggle = dataset_combined_checkbox_button.active
+    datasets: List[int] = dataset_checkbox_button.active
+    ip_and_sec_toggle: List[int] = dataset_combined_checkbox_button.active
 
     # Group combinations of toggles
-    ip_bool = Dataset.IP.value in datasets  # bool
-    sec_bool = Dataset.SEC.value in datasets  # bool
+    ip_bool: bool = Dataset.IP.value in datasets
+    sec_bool: bool = Dataset.SEC.value in datasets
 
-    ip_only = (ip_bool) and (not sec_bool)  # bool
-    sec_only = (sec_bool) and (not ip_bool)  # bool
-    ip_or_sec = (ip_bool) and (sec_bool)  # bool
-    ip_and_sec = CombinedDataset.IP_and_SEC.value in ip_and_sec_toggle  # bool
+    ip_only: bool = (ip_bool) and (not sec_bool)
+    sec_only: bool = (sec_bool) and (not ip_bool)
+    ip_or_sec: bool = (ip_bool) and (sec_bool)
+    ip_and_sec: bool = CombinedDataset.IP_and_SEC.value in ip_and_sec_toggle
 
     # A mask tells us which rows of the dataframe (df) to return
-    ip_mask = df["Interaction_support"] == "IP"  # Series
-    sec_mask = df["Interaction_support"] == "SEC"  # Series
-    both_mask = df["Interaction_support"] == "both"  # Series
+    ip_mask: Series = df["Interaction_support"] == "IP"
+    sec_mask: Series = df["Interaction_support"] == "SEC"
+    both_mask: Series = df["Interaction_support"] == "both"
 
     # Depending on the toggle, we return a different union of masks
     if ip_only:
@@ -130,7 +129,7 @@ def subset_by_edge_type(df: DataFrame) -> DataFrame:
     """
     Subset df to bait-prey pairs based on protein interaction type
     """
-    df_copy = df.copy()
+    df_copy: DataFrame = df.copy()
 
     # Note: DashPatterns take iterable as [dash, gap] ([] is a solid line)
     df_copy["edge_style"] = df_copy["IP_interaction_type"].map({
@@ -147,8 +146,8 @@ def subset_by_edge_type(df: DataFrame) -> DataFrame:
         "both": EdgeColors.Both.value
     })
 
-    interaction_type = interaction_type_checkbox_button.active  # List[int]
-    types_to_include = []  # List[Union[str, float]]
+    interaction_type: List[int] = interaction_type_checkbox_button.active
+    types_to_include: List[Union[str, float]] = []
 
     if len(interaction_type) == 0:
         return df
@@ -168,16 +167,15 @@ def subset_by_edge_type(df: DataFrame) -> DataFrame:
     types_to_include.append(nan)
 
     # Only consider edges with IP or IP & SEC for IP interaction type filtering
-    ip_mask = df_copy["Interaction_support"].isin(["IP", "both"])  # Series
-    ip_type_mask = df_copy[ip_mask]["IP_interaction_type"].isin(
-        types_to_include)
-    ip_rows = df_copy[ip_mask][ip_type_mask].copy()  # DataFrame
+    ip_mask: Series = df_copy["Interaction_support"].isin(["IP", "both"])
+    ip_type_mask: Series = df_copy[ip_mask]["IP_interaction_type"].isin(types_to_include)
+    ip_rows: DataFrame = df_copy[ip_mask][ip_type_mask].copy()
 
     # Get SEC rows separately
-    sec_mask = df_copy["Interaction_support"] == "SEC"  # Series
-    sec_rows = df_copy[sec_mask].copy()  # DataFrame
+    sec_mask: Series = df_copy["Interaction_support"] == "SEC"
+    sec_rows: DataFrame = df_copy[sec_mask].copy()
 
-    filtered_df = concat([ip_rows, sec_rows], axis=0)  # DataFrame
+    filtered_df: DataFrame = concat([ip_rows, sec_rows], axis=0)
     return filtered_df
 
 
@@ -211,8 +209,8 @@ def subset_by_protein(df: DataFrame) -> DataFrame:
     """
     Subset df to bait proteins list specified in protein_text_input
     """
-    protein_str = str(controls.protein_text_input.value).strip()  # str
-    protein_list = [p.strip() for p in protein_str.split(",")]  # List[str]
+    protein_str: str = str(controls.protein_text_input.value).strip()
+    protein_list: List[str] = [p.strip() for p in protein_str.split(",")]
 
     # If there's no proteins entered, we return all edges
     if (len(protein_list) == 1) and (protein_list[0] == ""):
@@ -242,19 +240,17 @@ def subset_by_protein(df: DataFrame) -> DataFrame:
 
 
 def lifecycle_node_attributes(df: DataFrame) -> Dict[Hashable, Dict[str, str]]:
-    bait_col = "Bait_lifecycle_step"  # str
-    prey_col = "prey_lifecycle_stage_main_by_most_common_bait_stage"  # str
+    bait_col: str = "Bait_lifecycle_step"
+    prey_col: str = "prey_lifecycle_stage_main_by_most_common_bait_stage"
 
     # Colors for bait proteins
-    bait_lifecycle_df = df[["Bait", bait_col]].copy()  # DataFrame
-    bait_lifecycle_df.dropna(inplace=True)  # None
-    bait_lifecycle_df.drop_duplicates(keep="first", inplace=True)  # None
-    bait_lifecycle_df.set_index("Bait", inplace=True)  # DataFrame
+    bait_lifecycle_df: DataFrame = df[["Bait", bait_col]].copy()
+    bait_lifecycle_df.dropna(inplace=True)
+    bait_lifecycle_df.drop_duplicates(keep="first", inplace=True)
+    bait_lifecycle_df.set_index("Bait", inplace=True)
 
-    # TODO: Fix autoformatter putting type hints on new line when >80 chars
-    # Dict[str, Dict[str, str]]
-    bait_lifecycle_dict = bait_lifecycle_df.to_dict()
-    bait_lifecycle_dict = bait_lifecycle_dict[bait_col]  # Dict[str, str]
+    _bait_lifecycle_dict: Dict[str, Dict[str, str]] = bait_lifecycle_df.to_dict()
+    bait_lifecycle_dict: Dict[str, str] = _bait_lifecycle_dict[bait_col]
 
     # Colors for prey proteins
     prey_lifecycle_df = df[["Prey", prey_col]].copy()
@@ -262,24 +258,23 @@ def lifecycle_node_attributes(df: DataFrame) -> Dict[Hashable, Dict[str, str]]:
     prey_lifecycle_df.drop_duplicates(keep="first", inplace=True)
     prey_lifecycle_df.set_index("Prey", inplace=True)
 
-    # Dict[str, Dict[str, str]]
-    prey_lifecycle_dict = prey_lifecycle_df.to_dict()
-    prey_lifecycle_dict = prey_lifecycle_dict[prey_col]  # Dict[str, str]
+    _prey_lifecycle_dict: Dict[str, Dict[str, str]] = prey_lifecycle_df.to_dict()
+    prey_lifecycle_dict: Dict[str, str] = _prey_lifecycle_dict[prey_col]
 
     # The | operation merges two dictionaries by keys
-    protein_dict = bait_lifecycle_dict | prey_lifecycle_dict  # Dict[str, str]
+    protein_dict: Dict[str, str] = bait_lifecycle_dict | prey_lifecycle_dict
 
-    node_attributes = {}  # Dict[Hashable, Dict[str, str]]
+    node_attributes: Dict[Hashable, Dict[str, str]] = {}
     for protein, lifecycle_step in protein_dict.items():
         if lifecycle_step in LifecycleColorsDict:
             node_color = LifecycleColorsDict[lifecycle_step]
         else:
             node_color = LifecycleColors.undetermined.value
 
-        attributes = {
+        attributes: Dict[str, str] = {
             "legend_label": lifecycle_step,
             "node_color": node_color
-        }  # Dict[str, str]
+        }
         node_attributes[protein] = attributes
 
     return node_attributes
@@ -300,37 +295,35 @@ def location_node_attributes(df: DataFrame) -> Dict[Hashable, Dict[str, str]]:
     unique_bait_locations = set(bait_location_dict.values())
 
     # Get location annotations for preys
-    prey_location_df = df[["Prey", prey_col]].copy()  # DataFrame
+    prey_location_df: DataFrame = df[["Prey", prey_col]].copy()
     prey_location_df.dropna(inplace=True)
     prey_location_df.drop_duplicates(keep="first", inplace=True)
     prey_location_df.set_index("Prey", inplace=True)
 
-    # Dict[str, Dict[str, str]]
-    prey_location_dict = prey_location_df.to_dict()
-    prey_location_dict = prey_location_dict[prey_col]  # Dict[str, str]
-    unique_prey_locations = set(prey_location_dict.values())  # Set[str]
+    _prey_location_dict: Dict[str, Dict[str, str]] = prey_location_df.to_dict()
+    prey_location_dict: Dict[str, str] = _prey_location_dict[prey_col]
+    unique_prey_locations: Set[str] = set(prey_location_dict.values())
 
     # The | operator merges dictionaries based on keys
-    # Dict[str, str]
-    combined_location_dict = bait_location_dict | prey_location_dict
+    combined_location_dict: Dict[str, str] = bait_location_dict | prey_location_dict
 
-    unique_locations = list(unique_bait_locations |
-                            unique_prey_locations)  # List[str]
+    unique_locations: List[str] = list(unique_bait_locations |
+                                       unique_prey_locations)
 
     # integer --> color in RGBA
-    color_map = cm.get_cmap("tab10", len(unique_locations))  # Colormap
+    color_map: cm.Colormap = cm.get_cmap("tab10", len(unique_locations))
 
     # location --> color
-    color_dict = {location: color_map(i)
-                  for i, location in enumerate(unique_locations)}
+    color_dict: Dict[str, str] = {location: color_map(i)
+                                  for i, location in enumerate(unique_locations)}
 
     # TODO: Make a legend for the disease coloring
 
     # node --> {"node_attribute": color}
-    node_attributes = {
+    node_attributes: Dict[Hashable, Dict[str, str]] = {
         key: {"node_color": rgba_to_hex(*color_dict[value])}
         for key, value in combined_location_dict.items()
-    }  # Dict[Hashable, Dict[str, str]]
+    }
 
     return node_attributes
 
@@ -345,9 +338,9 @@ def disease_node_attribute(df: DataFrame) -> Dict[Hashable, Dict[str, str]]:
     bait_disease_df.drop_duplicates(keep="first", inplace=True)
     bait_disease_df.set_index("Bait", inplace=True)
 
-    bait_disease_dict = bait_disease_df.to_dict()  # Dict[str, Dict[str, str]]
-    bait_disease_dict = bait_disease_dict[bait_col]  # Dict[str, str]
-    unique_bait_diseases = set(bait_disease_dict.values())
+    _bait_disease_dict: Dict[str, Dict[str, str]] = bait_disease_df.to_dict()
+    bait_disease_dict: Dict[str, str] = _bait_disease_dict[bait_col]
+    unique_bait_diseases: Set[str] = set(bait_disease_dict.values())
 
     # Get disease annotations for preys
     prey_disease_df = df[["Prey", prey_col]].copy()
@@ -355,49 +348,48 @@ def disease_node_attribute(df: DataFrame) -> Dict[Hashable, Dict[str, str]]:
     prey_disease_df.drop_duplicates(keep="first", inplace=True)
     prey_disease_df.set_index("Prey", inplace=True)
 
-    prey_disease_dict = prey_disease_df.to_dict()  # Dict[str, Dict[str, str]]
-    prey_disease_dict = prey_disease_dict[prey_col]  # Dict[str, str]
-    unique_prey_diseases = set(prey_disease_dict.values())
+    _prey_disease_dict: Dict[str, Dict[str, str]] = prey_disease_df.to_dict()
+    prey_disease_dict: Dict[str, str] = _prey_disease_dict[prey_col]
+    unique_prey_diseases: Set[str] = set(prey_disease_dict.values())
 
-    unique_diseases = list(unique_bait_diseases |
-                           unique_prey_diseases)  # List[str]
+    unique_diseases: List[str] = list(unique_bait_diseases |
+                                      unique_prey_diseases)
 
     # The | merges two dictionaries by keys
-    # Dict[str, str]
-    combined_disease_dict = bait_disease_dict | prey_disease_dict
+    combined_disease_dict: Dict[str, str] = bait_disease_dict | prey_disease_dict
 
     # integer --> color in RGBA
-    color_map = cm.get_cmap("tab10", len(unique_diseases))  # Colormap
+    color_map: cm.Colormap = cm.get_cmap("tab10", len(unique_diseases))
 
     # disease --> color
-    color_dict = {disease: color_map(i)
-                  for i, disease in enumerate(unique_diseases)}
+    color_dict: Dict[str, str] = {disease: color_map(i)
+                                  for i, disease in enumerate(unique_diseases)}
 
     # TODO: Make a legend for the disease coloring
 
     # node --> {"node_color": disease color}
     # Note: RGBA tuple -> rgba_to_hex(*RGBA) -> rgba_to_hex(R, G, B, A) -> hex
-    node_attributes = {
+    node_attributes: Dict[Hashable, Dict[str, str]] = {
         key: {"node_color": rgba_to_hex(*color_dict[value])}
         for key, value in combined_disease_dict.items()
-    }  # Dict[Hashable, Dict[str, str]]
+    }
 
     return node_attributes
 
 
 def determine_node_coloring(df: DataFrame) -> Dict[Hashable, Dict[str, str]]:
-    coloring_selection = controls.node_coloring_selection.value  # str
+    coloring_selection: str = controls.node_coloring_selection.value
 
     if coloring_selection == "none":
         baits = set(df["Bait"].unique())
         preys = set(df["Prey"].unique())
         proteins = baits | preys
 
-        no_attributes = {
+        no_attributes: Dict[Hashable, Dict[str, str]] = {
             protein: {"node_color": LifecycleColors.undetermined.value,
                       "legend_label": "Other"}
             for protein in proteins
-        }  # Dict[str, Dict[str, str]]
+        }
         return no_attributes
     elif coloring_selection == "lifecycle stage":
         lifecycle_attributes = lifecycle_node_attributes(df)
